3ce30544887c11da8a5ec7f5dec53827d23c182c
asiff <asiffmahmad9@gmail.com>
2026-08-22 16:21:23 +0530

    Fix Prisma ternary type error in lib/prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,9 +4,9 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient(
-    process.env.NODE_ENV === 'development' ? { log: ['query'] } : {}
-  );
+  (process.env.NODE_ENV === 'development'
+    ? new PrismaClient({ log: ['query'] })
+    : new PrismaClient());
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
